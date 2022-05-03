90db2f2631e82ab30dbbb94058ba798beb160959
Yash Prajapati <101260966+2002Yash@users.noreply.github.com>
2022-05-03 22:56:52 +0530

    Add files via upload

---
+++ BubbleSort.c
@@ -0,0 +1,49 @@
+// Bubble Sort
+
+#include <stdio.h>
+
+void bubbleSort(int array[], int size)
+{
+
+    for (int step = 0; step < size - 1; ++step)
+    {
+
+        for (int i = 0; i < size - step - 1; ++i)
+        {
+
+            if (array[i] > array[i + 1])
+            {
+
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int data[] = {-2, 45, 0, 11, -9};
+
+    int size = sizeof(data) / sizeof(data[0]);
+
+    printf("Unsorted Array: \n");
+    printArray(data, size);
+
+    printf("\nAfter applying Bubble Sort...\n");
+
+    bubbleSort(data, size);
+
+    printf("\nSorted Array in Ascending Order:\n");
+    printArray(data, size);
+}
